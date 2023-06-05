a67abaa122ce72f44d592e49c0c8c8180b31cfd0
thewhitelisted <clee2k6@outlook.com>
2023-06-05 10:45:14 -0400

    added support for python

--- main.c
+++ main.c
@@ -4,11 +4,12 @@
 #include <conio.h>
 
 int create_C(char name[]);
+int create_Python(char name[]);
 
 int main(void)
 {
     // variable initialize
-    system("cls");
+    // system("cls");
     char input[255];
     char folder_path[255];
     char file_path[255];
@@ -16,7 +17,7 @@ int main(void)
     while (1)
     {
         printf("Code File Creator\n\n");
-        printf("Commands: c, py\n\n");
+        printf("Commands: c, python\n\n");
         scanf("%s", input);
 
         // for c files
@@ -62,6 +63,48 @@ int main(void)
                 system("cls");
             }
         }
+        else if (!strcmp(input, "python"))
+        {
+            system("cls");
+            printf("PYTHON FILE CREATOR");
+
+            // get folder path
+            printf("\n\nENTER THE FOLDER PATH: ");
+            scanf("%s", folder_path);
+
+            // removes the slash at the end if they indluded it
+            if (folder_path[strlen(folder_path) - 1] == '\\')
+            {
+                folder_path[strlen(folder_path) - 1] = '\0';
+            }
+
+            // get file name and append it to the folder path
+            printf("\nENTER THE FILE NAME (DO NOT INCLUDE FILE EXTENSION): ");
+            scanf("%s", file_path);
+            strcat(strcat(folder_path, "\\"), file_path);
+
+            // if the function successfully creates the file, print statement
+            if (create_Python(folder_path) == 1)
+            {
+                printf("\nSuccessfully created file at %s.py", folder_path);
+                printf("\npress any key to continue");
+                getch();
+                system("cls");
+
+                // open vscode to the file
+                strcat(folder_path, ".py");
+                char cmd[] = "code ";
+                strcat(cmd, folder_path);
+                system(cmd);
+            }
+            else
+            {
+                printf("\nCould not create file");
+                printf("\npress any key to continue");
+                getch();
+                system("cls");
+            }
+        }
         else
         {
             system("cls");
@@ -84,6 +127,30 @@ int create_C(char name[])
 
     // write
     fputs("#include <stdio.h>\n\nint main()\n{\n\t\n\treturn 0;\n}", fp);
+
+    // check if the file has been created
+    FILE *file_check;
+    if (file_check = fopen(file_name, "r"))
+    {
+        fclose(fp);
+        fclose(file_check);
+        return 1;
+    }
+    return 0;
+}
+
+int create_Python(char name[])
+{
+    // setup file and file name
+    char file_name[255];
+    const char file_extension[] = ".py";
+    strcpy(file_name, name);
+    strcat(file_name, file_extension);
+    FILE *fp;
+    fp = fopen(file_name, "w");
+
+    // write
+    fputs("def main(): -> None\n\tpass\n\nif __name__ == '__main__':\n\tmain()", fp);
 
     // check if the file has been created
     FILE *file_check;
